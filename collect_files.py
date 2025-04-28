8b9901bb49b06d7e5dd3cdf5e93d88d9a8edcafe
root <root@jkoufsbpcb.local>
2025-04-28 09:24:55 +0000

    1

--- collect_files.py
+++ collect_files.py
@@ -1,9 +1,1 @@
 
-import sys
-import os
-input_dir=sys.argv[1]
-output_dir=sys.argv[2]
-if not os.path.exists(input_dir) or not os.path.isdir(input_dir):
-    print("Не существует или не является директорией")
-if not os.path exists(output_dir) or not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
