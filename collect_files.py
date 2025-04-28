05d3f96f0b54fc9271de14f0dea9f0f352bd63f4
root <root@wvmatgfojr.local>
2025-04-28 08:35:41 +0000

    hhhhhhhhhhhh

--- collect_files.py
+++ collect_files.py
@@ -3,7 +3,7 @@ import sys
 import os
 input_dir=sys.argv[1]
 output_dir=sys.argv[2]
-if not os.path.exists('input_dir') or not os.path.isdir('input_dir'):
+if not os.path.exists(input_dir) or not os.path.isdir(input_dir):
     print("Не существует или не является директорией")
-if not os.path exists('output_dir') or not os.path.isdir('output_dir'):
-    os.mkdir('output_dir')
+if not os.path exists(output_dir) or not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
