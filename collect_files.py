0d9054a5676356e2765c1a686074ec61832819fa
root <root@jkoufsbpcb.local>
2025-04-28 10:33:35 +0000

    final

--- collect_files.py
+++ collect_files.py
@@ -1,1 +1,29 @@
+import os
+import sys
+import shtil
+# команды отсюда https://habr.com/ru/companies/selectel/articles/547290/
+input_dir=sys.argv[1]
+output_dir=sys.argv[2]
+if not os.path.exists(input_dir) or not os.path.exists(input_dir):
+    print("Не существует или не является директорией")
+    sys.exit(1)
+if not  os.path.exists(output_dir) or not os.path.exists(output_dir):
+    os.mkdirs(output_dir)
+def novoeimya(name, dest):
+    bs, ext = os.path.splitext(name)
+    cdd=name 
+    i=0
+    while os.path.exists(os.path.join(dest, cdd)):
+        i+=1
+        cdd=f"{bs}{i}{ext}"
+    return cdd
+# функция novoeimya https://stackoverflow.com/questions/13852700/create-file-but-if-name-exists-add-number#:~:text=def%20nextnonexistent,%28root%2C%20i%2C%20ext
+for root, dirs, files in os.walk(input_dir):
+    for filename in files:
+        input_dir=os.path.join(root, imyafile)
+        nname=novoeimya(imyafile, output_dir)
+        output_dir=os.path.join(output_indoor, nname)
+        shutil.copy2(input_dir, output_dir)
+# проход по всем папкам https://stackoverflow.com/questions/19777292/recursively-copy-and-flatten-a-directory-with-python/19777568#19777568
+# перенос файла https://docs.python.org/3/library/shutil.html
 
